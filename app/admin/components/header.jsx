52925c5f1433443da63650ccfb4ab0ff8bfe76d5
BIDU <bidufpv@gmail.com>
2024-12-19 01:03:46 +0530

    minor fix

--- app/admin/components/header.jsx
+++ app/admin/components/header.jsx
@@ -1,26 +1,3 @@
-// "use client";
-
-// import { Menu } from "lucide-react";
-
-
-// export default function Header({toggleSidebar}) {
-//   return (
-//     <section
-//       className="fixed w-full top-0 flex items-center gap-3
-//      bg-white border-b px-4 py-3"
-//     >
-//       <div className="md:hidden flex items-center">
-//         <button onClick={toggleSidebar}>
-//           <Menu className="w-6 h-6" />
-//         </button>
-//       </div>
-
-//       <h1 className="text-xl font-semibold">Dashboard</h1>
-//     </section>
-//   );
-// }
-
-
 "use client";
 import { Menu } from "lucide-react";
 
@@ -31,7 +8,7 @@ export default function Header({ toggleSideBar }) {
      bg-white border-b px-4 py-3"
     >
       {/* Menu Button for Mobile */}
-      <div className="block md:hidden">
+      <div className="md:hidden flex items-center">
         <button onClick={toggleSideBar}>
           <Menu className="w-6 h-6" />
         </button>
@@ -41,3 +18,4 @@ export default function Header({ toggleSideBar }) {
     </section>
   );
 }
+
